1b24819bdc5fe991d08907e0e124123351ddecf5
Divashka <dianakuzmina325@gmail.com>
2024-01-16 22:27:09 +0300

    Подготовка проекта и страниц

---
+++ src/components/app/app.tsx
@@ -0,0 +1,31 @@
+import { HelmetProvider } from 'react-helmet-async';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { AppRoute } from '../../const/const';
+import Catalog from '../../pages/catalog/catalog';
+import Basket from '../../pages/basket/basket';
+import Product from '../../pages/product/product';
+
+function App(): JSX.Element {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<Catalog />}
+          />
+          <Route
+            path={AppRoute.Basket}
+            element={<Basket />}
+          />
+          <Route
+            path={AppRoute.Product}
+            element={<Product />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+}
+
+export default App;
