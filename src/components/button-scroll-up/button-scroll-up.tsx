d0ed74c5485bc7a8174ac64a5a9f7df4fc053996
Divashka <dianakuzmina325@gmail.com>
2024-01-25 15:07:08 +0300

    Реализация кнопки "наверх"

--- src/components/button-scroll-up/button-scroll-up.tsx
+++ src/components/button-scroll-up/button-scroll-up.tsx
@@ -2,8 +2,20 @@
 import { memo } from 'react';
 
 function ButtonScrollUpComponent(): JSX.Element {
+
+  function handleUpButtonClick() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
   return (
-    <a className="up-btn" href="#header">
+    <a
+      className="up-btn"
+      onClick={handleUpButtonClick}
+    >
       <svg width="12" height="18" aria-hidden="true">
         <use xlinkHref="#icon-arrow2"></use>
       </svg>
